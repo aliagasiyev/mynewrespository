0e2b0acf49c1e16194dfc0c27645b6b485f88133
aliagasiyev <agasiyevali2@gmail.com>
2025-02-15 20:49:46 +0400

    first project attached

---
+++ src/main/java/az/edu/turing/module1/lesson03/MovieApp.java
@@ -0,0 +1,7 @@
+package az.edu.turing.module1.lesson03;
+
+public class MovieApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/az/edu/turing/module1/lesson04/TestApp.java
@@ -0,0 +1,7 @@
+package az.edu.turing.module1.lesson04;
+
+public class TestApp {
+    public static void main(String[] args) {
+
+    }
+}
